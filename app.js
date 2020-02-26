adeb898f4b74330e852564fa9e96fabf48b999e3
Marcos Corona <mcorona.avalos02@gmail.com>
2020-02-26 12:06:23 -0300

    Add helmet

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const helmet = require('helmet');
 const api = require('api');
 
 const app = express();
+
+app.use(helmet());
+
+app.use(helmet.referrerPolicy())
+
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"]
+  }
+}));
+
+app.use(helmet.featurePolicy({
+  features: {
+    fullscreen: ["'self'"],
+    vibrate: ["'none'"],
+    syncXhr: ["'none'"]
+  }
+}));
 
 app.use(express.json());
 
