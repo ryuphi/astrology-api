9bdc263eaf90518d81efd31f3c9b433ffbbfc834
Marcos Corona <mcorona.avalos02@gmail.com>
2020-04-18 21:32:44 -0400

    Small refactor in api/index.js

--- src/api/index.js
+++ src/api/index.js
@@ -5,19 +5,17 @@ const astrologer = require('astrologer');
 router.get('/', async (req, res) => res.status(200).json({ message: 'Welcome to Astrology api!' }));
 
 router.post('/ephemeris', async (req, res) => {
-  const planets = {};
-
-  await Object.keys(astrologer.PLANETS).forEach(async (item) => {
-    const pos = await astrologer.position(item, new Date(req.body.time));
-    planets[item] = {
+  // resolve all promise..
+  const astros = await Promise.all(Object.keys(astrologer.PLANETS).map(async item => {
+    return {
       name: item,
-      ...pos
+      ...await astrologer.position(item, new Date(req.body.time))
     };
-  });
+  }));
 
   res.status(200).json({
     data: {
-      astros: planets
+      astros,
     }
   });
 });
