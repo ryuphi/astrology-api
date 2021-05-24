2b15128e411d6f8e70d91b9ff0e25affb4824ebc
Marcos Corona <mcorona.avalos02@gmail.com>
2021-05-24 04:07:24 -0400

    add missing test to astrologer.aspects fn

---
+++ test/units/calculate-aspects-of-list-of-planets.test.js
@@ -0,0 +1,26 @@
+const astrologer = require("./../../src/astrologer");
+
+describe("calculate aspects of planets list", () => {
+  const planetList = {
+    sun: {
+      name: "sun",
+      type: "luminary",
+      position: {
+        longitude: 30,
+      },
+    },
+    pluto: {
+      name: "pluto",
+      type: "transpersonal",
+      position: {
+        longitude: 30,
+      },
+    }
+  };
+
+  test("calculate aspects using astrologer.aspects", () => {
+    const aspects = astrologer.aspects(planetList);
+    expect(aspects.sun.length).toBe(1);
+    expect(aspects.sun[0].name).toBe("conjunction");
+  });
+});
