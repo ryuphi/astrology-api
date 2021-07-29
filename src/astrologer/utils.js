b05d76f3435ca3a9d069845a66b23ac7ff4f8a2c
Marcos Corona <mcorona.avalos02@gmail.com>
2021-07-28 22:44:41 -0400

    fix(houses): change calc base into utc to calc base into calendar day
    Previously, this implementation was using the recommended methods for
    dates based on Coordinated Universal Time (UTC). These functions are
    recommended more for hours obtained from clock. The problem with them
    is that for some calculations they deliver differences with other
    libraries that use swesseph. Due to standardization issues with other
    libraries and to avoid confusion, it was decided to replace the
    implementation to be able to use the dates based on the calendar day.

--- src/astrologer/utils.js
+++ src/astrologer/utils.js
@@ -1,21 +1,24 @@
 const swisseph = require("swisseph");
 
 const utcToJulianUt = (utcDate) => {
-  const { julianDayUT } = swisseph.swe_utc_to_jd(
+  const milliSecondsInSeconds = utcDate.getUTCMilliseconds() / 1000;
+  const secondsInMinutes = (utcDate.getUTCSeconds() + milliSecondsInSeconds) / 60;
+  const minutesInHours = (utcDate.getUTCMinutes() + secondsInMinutes) / 60;
+
+  const hours = utcDate.getUTCHours() + minutesInHours;
+
+  return swisseph.swe_julday(
     utcDate.getUTCFullYear(),
     utcDate.getUTCMonth() + 1,
     utcDate.getUTCDate(),
-    utcDate.getUTCHours(),
-    utcDate.getUTCMinutes(),
-    utcDate.getUTCSeconds(),
+    hours,
     swisseph.SE_GREG_CAL
   );
-
-  return julianDayUT;
 };
 
 const degreesToDms = (value) => {
-  const { degree: degrees, min: minutes, second: seconds } = swisseph.swe_split_deg(value, swisseph.SE_SPLIT_DEG_ZODIACAL);
+  const position = swisseph.swe_split_deg(value, swisseph.SE_SPLIT_DEG_ZODIACAL);
+  const { degree: degrees, min: minutes, second: seconds } = position;
 
   return {
     degrees,
